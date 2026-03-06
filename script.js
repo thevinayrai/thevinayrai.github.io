d0124e433920641d56b581cacd6e17423948112c
Vinay Kumar Rai <vinayrai4444@gmail.com>
2026-03-06 16:53:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typing", {
+strings: [
+"Full Stack Developer",
+"JavaScript Developer",
+"React Developer",
+"Problem Solver"
+],
+typeSpeed: 60,
+backSpeed: 40,
+loop: true
+});
